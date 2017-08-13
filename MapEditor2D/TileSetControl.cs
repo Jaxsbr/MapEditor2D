02c3fa0c77888f183b0eabaf02f803283be97b72
Unknown <jbrink386@gmail.com>
2017-08-13 17:05:36 +0200

    Mouse selection logic added

--- MapEditor2D/TileSetControl.cs
+++ MapEditor2D/TileSetControl.cs
@@ -16,9 +16,15 @@ namespace MapEditor2D
         private Map _map;
         private Image _image;
 
+        private Rectangle _prevSelectedTile;
+        private Rectangle _selectedTile;
+        private List<Rectangle> _selectedRectangles = new List<Rectangle>();
+
+
         public TileSetControl()
         {
             InitializeComponent();
+            DoubleBuffered = true;
         }
 
         public void InitMap(Map map)
@@ -36,6 +42,7 @@ namespace MapEditor2D
             if (_map != null && _map.TileSet != null)
             {
                 DrawTileSet(e);
+                DrawSelectedTiles(e);
             }
         }
 
@@ -43,6 +50,57 @@ namespace MapEditor2D
         {
             base.OnResize(e);
             Invalidate(); // TODO: Rectangle of visible area
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+
+            var tileCoords = GetTileUnderMouse(e.Location);
+            _prevSelectedTile = _selectedTile;
+            _selectedTile = new Rectangle(
+                tileCoords.X * _map.TileWidth,
+                tileCoords.Y * _map.TileHeight,
+                _map.TileWidth,
+                _map.TileHeight);
+
+            Invalidate();
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+           base.OnMouseMove(e);
+
+            if (e.Button != MouseButtons.Left)
+            {
+                return;
+            }
+
+            var tileCoords = GetTileUnderMouse(e.Location);
+            var tileRect = new Rectangle(
+                tileCoords.X * _map.TileWidth,
+                tileCoords.Y * _map.TileHeight,
+                _map.TileWidth,
+                _map.TileHeight);
+
+            if (!_selectedRectangles.Contains(tileRect))
+            {
+                _selectedRectangles.Add(tileRect);
+                Invalidate();
+            }
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            _selectedRectangles.Clear();
+        }
+
+        private Point GetTileUnderMouse(Point mousePoint)
+        {
+            var x = Math.Floor((double)mousePoint.X / _map.TileWidth);
+            var y = Math.Floor((double)mousePoint.Y / _map.TileHeight);
+            return new Point((int)x, (int)y);
         }
 
         private void DrawTileSet(PaintEventArgs e)
@@ -69,25 +127,16 @@ namespace MapEditor2D
                 row * _map.TileHeight,
                 _map.TileWidth,
                 _map.TileHeight);
+        }
 
-            //var destRect = new Rectangle(
-            //    col * _map.TileWidth,
-            //    row * _map.TileHeight,
-            //    _map.TileWidth,
-            //    _map.TileHeight);
+        private void DrawSelectedTiles(PaintEventArgs e)
+        {
+            var brush = new SolidBrush(Color.FromArgb(128, Color.CornflowerBlue));
 
-            //var srcRect = new Rectangle(
-            //    col * _map.TileWidth, 
-            //    row * _map.TileHeight,
-            //    _map.TileWidth,
-            //    _map.TileHeight
-            //);
-
-            //e.Graphics.DrawImage(
-            //    _image,
-            //    destRect,
-            //    srcRect,
-            //    GraphicsUnit.Pixel);
+            foreach (var rect in _selectedRectangles)
+            {
+                e.Graphics.FillRectangle(brush, rect);
+            }            
         }
     }
 }
